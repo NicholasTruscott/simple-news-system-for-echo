fbaae0a3c9011fea244fb02cfa9dfd8f911ab6a6
NicholasTruscott <47369452+NicholasTruscott@users.noreply.github.com>
2019-02-05 21:48:24 +0000

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,10 @@
+<?
+// conf.php - configuration parameters
+
+// database configuration
+$host = "****"; //insert host name
+$db = "****"; //insert database name
+$user = "****"; //insert database user name
+$pass = "****"; //insert database password
+
+?>
